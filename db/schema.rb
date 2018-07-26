6ad2d93f7f57635468bc06be30cf5c57fc561b86
maatcha <mattcha54@gmail.com>
2018-07-26 08:30:22 +0200

    full_tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_25_212204) do
+ActiveRecord::Schema.define(version: 2018_07_25_233646) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_articles_on_author_id"
   end
 
   create_table "categories", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#gem faker pour générer des listes factices
+ require 'faker'
+
+#Création de 10 user
+
+# 10.times do
+# 	user = User.create(first_name: Faker::DragonBall.character, last_name: Faker::Name.last_name, email: Faker::Internet.email)
+# end
+
+
+#Création de 10 article
+
+10.times do
+	article = Article.create(title: Faker::BackToTheFuture.date, content: "#{Faker::Community.quotes}")
+end
+
+5.times do
+	categorie = Categorie.create(name: Faker::Cat.name)
+end
+
+15.times do
+	comment = Comment.create(content: Faker::BojackHorseman.tongue_twister)
+end
+
